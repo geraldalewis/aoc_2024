70ada1284c131cb77d6eff77667f86d719e141e1
Gerald A Lewis <gerald.a.lewis@gmail.com>
2024-12-03 18:31:08 -0500

    day 1 less funny

--- 01/01.go
+++ 01/01.go
@@ -6,32 +6,9 @@ import (
 	"sort"
 	"strconv"
 	"strings"
-	"unsafe"
 )
 
-func absInt(x int) uint {
-	bits := *(*uint64)(unsafe.Pointer(&x))
-	isNegative := (bits & (1 << 63)) == (1 << 63)
-	if isNegative {
-		return uint(^x + 1)
-	}
-	return uint(x)
-}
-
 func main() {
-	var rawBits uint64 = 0b1111111111111111111111111111111111111111111111111111111111111111
-
-	fmt.Printf("%x\n", rawBits)
-
-	var f int = 42
-	bits := *(*uint64)(unsafe.Pointer(&f))
-	bitsShifted := bits & (1 << 63)
-	isNegative := (bits & (1 << 63)) == (1 << 63)
-
-	fmt.Printf("Raw bits as x: %064b\n", bits)
-	fmt.Printf("bitsShifted: %064b\n", bitsShifted)
-	fmt.Printf("isNegative %t\n", isNegative)
-
 	data, err := os.ReadFile("01.data")
 	if err != nil {
 		panic("Could not read file")
@@ -53,19 +30,19 @@ func main() {
 			}
 		}
 	}
+	if len(colA) != len(colB) {
+		panic("Lists of ints aren't the same length")
+	}
 	sort.Ints(colA)
 	sort.Ints(colB)
-	var diffs []int
+	total := 0
 	for i, num := range colA {
-		// numA := float64(num)
-		// numB := float64(colB[i])
-		// diff := math.Abs(numA - numB)
-		diff := absInt(num - colB[i])
-		diffs = append(diffs, int(diff))
-	}
-	var total = 0
-	for _, num := range diffs {
-		total += num
+		a, b := num, colB[i]
+		if a > b {
+			total += a - b
+		} else {
+			total += b - a
+		}
 	}
 	fmt.Printf("%+v\n", total)
 }
